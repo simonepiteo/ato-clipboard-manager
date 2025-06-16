8c4babb8c51f01fbb2c7fa67db672382155dff95
Simone Piteo <simone.piteo-cic@ibm.com>
2025-06-16 21:11:35 +0200

    feat: menu bar structure

--- macos/clipboardManager-macOS/AppDelegate.h
+++ macos/clipboardManager-macOS/AppDelegate.h
@@ -2,5 +2,7 @@
 #import <Cocoa/Cocoa.h>
 
 @interface AppDelegate : RCTAppDelegate
-
+    @property(strong, nonatomic) NSPopover *popover;
+    @property(strong, nonatomic) NSStatusItem *statusItem;
+    @property(strong, nonatomic) NSString *jsBundleURLForBundleRoot;
 @end
--- macos/clipboardManager-macOS/AppDelegate.mm
+++ macos/clipboardManager-macOS/AppDelegate.mm
@@ -1,45 +1,44 @@
 #import "AppDelegate.h"
-
+#import <React/RCTBridge.h>
+#import <React/RCTRootView.h>
 #import <React/RCTBundleURLProvider.h>
+#import <Cocoa/Cocoa.h>
 
 @implementation AppDelegate
 
-- (void)applicationDidFinishLaunching:(NSNotification *)notification
-{
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+  
   self.moduleName = @"clipboardManager";
-  // You can add your custom initial props in the dictionary below.
-  // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
-
-  return [super applicationDidFinishLaunching:notification];
+  self.jsBundleURLForBundleRoot = @"index";
+  
+  NSURL *jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:self.jsBundleURLForBundleRoot];
+  RCTBridge *bridge = [[RCTBridge alloc] initWithBundleURL:jsCodeLocation moduleProvider:nil launchOptions:nil];
+  RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge moduleName:self.moduleName initialProperties:self.initialProps];
+  
+  NSViewController *contentViewController = [NSViewController new];
+  contentViewController.view = rootView;
+  
+  self.popover = [[NSPopover alloc] init];
+  self.popover.contentViewController = contentViewController;
+  self.popover.behavior = NSPopoverBehaviorTransient;
+  
+  self.statusItem = [[NSStatusBar systemStatusBar]
+                     statusItemWithLength:NSVariableStatusItemLength];
+  
+  self.statusItem.button.title = @"📋";
+  self.statusItem.button.target = self;
+  self.statusItem.button.action = @selector(togglePopover:);
 }
 
-- (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
-{
-  return [self bundleURL];
-}
-
-- (NSURL *)bundleURL
-{
-#if DEBUG
-  return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index"];
-#else
-  return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
-#endif
-}
-
-/// This method controls whether the `concurrentRoot`feature of React18 is turned on or off.
-///
-/// @see: https://reactjs.org/blog/2022/03/29/react-v18.html
-/// @note: This requires to be rendering on Fabric (i.e. on the New Architecture).
-/// @return: `true` if the `concurrentRoot` feature is enabled. Otherwise, it returns `false`.
-- (BOOL)concurrentRootEnabled
-{
-#ifdef RN_FABRIC_ENABLED
-  return true;
-#else
-  return false;
-#endif
+- (void)togglePopover:(id)sender {
+  if (self.popover.isShown) {
+    [self.popover performClose:sender];
+  } else {
+    [self.popover showRelativeToRect:[self.statusItem.button bounds]
+                              ofView:self.statusItem.button
+                       preferredEdge:NSRectEdgeMinY];
+  }
 }
 
 @end
