988953b43af9894e86fb830a0128cad81671f389
Simone Piteo <simone.piteo-cic@ibm.com>
2025-06-20 20:41:15 +0200

    feat: display mode setting

--- components/SingleNote/SingleNote.tsx
+++ components/SingleNote/SingleNote.tsx
@@ -4,6 +4,8 @@ import {SingleNoteProps} from './SingleNote.model';
 import {copyItem} from '../../utils/CopyItem';
 import {useInteractiveElements} from '../../hooks/useInteractiveElements';
 import {useSettings} from '../../hooks/useSetting';
+import {displayModeData} from '../../utils/SettingsData';
+import {defaultSettings} from '../../utils/Settings';
 
 const SingleNote: React.FC<SingleNoteProps> = ({item, id}) => {
   const {
@@ -25,6 +27,56 @@ const SingleNote: React.FC<SingleNoteProps> = ({item, id}) => {
       WindowManager.closePopover(true);
     }
   };
+
+  const style = StyleSheet.create({
+    singleNote: {
+      width:
+        displayModeData[settings?.displayMode ?? defaultSettings.displayMode],
+      height: 98,
+      alignItems: 'flex-start',
+      justifyContent: 'flex-start',
+      borderRadius: 5,
+      borderWidth: 1,
+      borderColor: 'rgba(204,204,204,.3)',
+      backgroundColor: 'rgba(0, 0, 0, 0.1)',
+      borderStyle: 'solid',
+      padding: 4,
+      cursor: 'pointer',
+    },
+    singleNoteHovered: {
+      borderColor: '#007AFF',
+    },
+    singleNoteFocused: {
+      backgroundColor: '#007AFF',
+    },
+    shortcutBadge: {
+      position: 'absolute',
+      bottom: 5,
+      right: 5,
+      borderRadius: 3,
+      backgroundColor: 'rgba(0, 0, 0, 0.4)',
+      padding: 2,
+      paddingEnd: 4,
+      paddingStart: 4,
+    },
+    shortcutText: {
+      fontSize: 11,
+      color: 'rgba(255,255,255,.7)',
+    },
+    textItem: {margin: 3},
+    imageContainer: {
+      flex: 1,
+      alignSelf: 'stretch',
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    imageItem: {
+      width: '100%',
+      height: '100%',
+      borderRadius: 3,
+      resizeMode: 'contain',
+    },
+  });
 
   return (
     <View
@@ -55,54 +107,5 @@ const SingleNote: React.FC<SingleNoteProps> = ({item, id}) => {
     </View>
   );
 };
-
-const style = StyleSheet.create({
-  singleNote: {
-    width: 98,
-    height: 98,
-    alignItems: 'flex-start',
-    justifyContent: 'flex-start',
-    borderRadius: 5,
-    borderWidth: 1,
-    borderColor: 'rgba(204,204,204,.3)',
-    backgroundColor: 'rgba(0, 0, 0, 0.1)',
-    borderStyle: 'solid',
-    padding: 4,
-    cursor: 'pointer',
-  },
-  singleNoteHovered: {
-    borderColor: '#007AFF',
-  },
-  singleNoteFocused: {
-    backgroundColor: '#007AFF',
-  },
-  shortcutBadge: {
-    position: 'absolute',
-    bottom: 5,
-    right: 5,
-    borderRadius: 3,
-    backgroundColor: 'rgba(0, 0, 0, 0.4)',
-    padding: 2,
-    paddingEnd: 4,
-    paddingStart: 4,
-  },
-  shortcutText: {
-    fontSize: 11,
-    color: 'rgba(255,255,255,.7)',
-  },
-  textItem: {margin: 3},
-  imageContainer: {
-    flex: 1,
-    alignSelf: 'stretch',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  imageItem: {
-    width: '100%',
-    height: '100%',
-    borderRadius: 3,
-    resizeMode: 'contain',
-  },
-});
 
 export default SingleNote;
---
+++ utils/SettingsData.ts
@@ -0,0 +1,4 @@
+export const displayModeData = {
+  grid: 98,
+  list: 310,
+};
