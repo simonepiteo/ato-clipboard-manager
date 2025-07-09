9039194250258a2794b465a93bd6908e29268845
Simone Piteo <simone.piteo-cic@ibm.com>
2025-07-09 22:02:09 +0200

    fix: babel

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,3 @@
 module.exports = {
-  presets: [
-    'module:@react-native/babel-preset',
-    'module:metro-react-native-babel-preset',
-  ],
+  presets: ['module:@react-native/babel-preset'],
 };
--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,3 @@
 module.exports = {
   preset: 'react-native',
-  transform: {
-    '^.+\\.[jt]sx?$': 'babel-jest',
-  },
-  transformIgnorePatterns: [
-    'node_modules/(?!(react-native|react-native-macos|@react-native|@react-native-macos)/)',
-  ],
-  // If you use TypeScript, add:
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
 };
